f7c24378682f9f92036b48be29a148557cfecc25
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-06 12:09:51 -0700

    added +2 and reset functionalities

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
 app.secret_key = 'secret'
-count = 0
 
 @app.route('/')
 def index():
-    global count
-    count += 1
-    session['count'] = count
+    if session.get('count'):
+        session['count'] += 1
+    else:
+        session['count'] = 1
     return render_template('index.html')
+
+@app.route('/add2', methods=['POST'])
+def add2():
+    session['count'] += 1
+    return redirect('/')
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    session['count'] = 0
+    return redirect('/')
 
 app.run(debug=True)
